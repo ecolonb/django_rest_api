6ccd025e6495beb6d5eccf75e94dadbde9be44ee
Edd Colbar <edgar.colonb@gmail.com>
2019-07-20 15:01:54 -0500

    Actualizando .gitignore

--- .env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/edgarcolonbarrientos/Documents/projects/django/rest_api_envio_click/.env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
